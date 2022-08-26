907940cd736ee77c3d5de50f4711ed4874a9a871
Andrew Broderick <mjb1504@rit.edu>
2022-08-26 14:33:16 -0400

    E01b

---
+++ E01b/main.cpp
@@ -0,0 +1,13 @@
+/* ---- Comments about this program*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	// Ending the program
+	cout << "Enter any key to end the program:";
+	getchar();
+	return 0;
+}
