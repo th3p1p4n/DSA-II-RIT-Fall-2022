b7ced73badcd92c62ca35a82cd679c96be6a9906
Andrew Broderick <mjb1504@rit.edu>
2022-11-14 22:37:45 -0500

    finished A4 robot rig :D

--- A4_rigged-robot/main.cpp
+++ A4_rigged-robot/main.cpp
@@ -25,9 +25,17 @@ const float PI = 3.1415926;
 bool keyStates[256];
 int buttonState;
 
+// ids for each body part
 int id = -1;
+
+// which body part is currently selected. starts with pelvis as default.
 int selectedId = 0;
-float rotations[16] = { PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, PI / 2, PI / 2, PI / 2, PI, PI, PI, 0.0, 0.0, 0.0 };
+
+// the initial orientations of the shapes, used for drawing
+float orientations[16] = { PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, PI / 2, PI / 2, PI / 2, PI, PI, PI, 0.0, 0.0, 0.0 };
+
+// the modifiable rotations
+float rotations[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
 void init(void)
 {
@@ -54,7 +62,7 @@ void drawBox(float boxWidth, float boxHeight, float rotation, bool selected)
     if (selected) { // use a different fill color if the box is selected
         glColor3f(0.2, 0.0, 0.8);
     }
-    glVertex2f(a[0] * cos(rotation) - a[1] * sin(rotation), a[0]*sin(rotation)+a[1]*cos(rotation));
+    glVertex2f(a[0] * cos(rotation) - a[1] * sin(rotation), a[0] * sin(rotation) + a[1] * cos(rotation));
     glVertex2f(b[0] * cos(rotation) - b[1] * sin(rotation), b[0] * sin(rotation) + b[1] * cos(rotation));
     glVertex2f(c[0] * cos(rotation) - c[1] * sin(rotation), c[0] * sin(rotation) + c[1] * cos(rotation));
     glVertex2f(d[0] * cos(rotation) - d[1] * sin(rotation), d[0] * sin(rotation) + d[1] * cos(rotation));
@@ -91,75 +99,91 @@ void display(void)
 
     //pelvis
     id = 0;
-    drawBox(2.5, 3.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.5, 3.0, orientations[id], id == selectedId);
     glPushMatrix();
     // left thigh
     id++;
     glTranslatef(-1.0, 0.0, 0.0);
-    drawBox(3.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(3.0, 1.0, orientations[id], id == selectedId);
     // left calf
     id++;
     glTranslatef(0.0, -3.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.0, 1.0, orientations[id], id == selectedId);
     // left foot
     id++;
     glTranslatef(0.0, -2.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.5, 1.5, orientations[id], id == selectedId);
     // right thigh
     id++;
     glPopMatrix();
     glPushMatrix();
     glTranslatef(1.0, 0.0, 0.0);
-    drawBox(3.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(3.0, 1.0, orientations[id], id == selectedId);
     // right calf
     id++;
     glTranslatef(0.0, -3.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.0, 1.0, orientations[id], id == selectedId);
     // right foot
     id++;
     glTranslatef(0.0, -2.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.5, 1.5, orientations[id], id == selectedId);
     // chest 
     id++;
     glPopMatrix();
     glTranslatef(0.0, 2.5, 0.0);
-    drawBox(2.7, 3.5, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.7, 3.5, orientations[id], id == selectedId);
     glPushMatrix();
     // neck
     id++;
     glTranslatef(0.0, 2.7, 0.0);
-    drawBox(1.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.0, 1.0, orientations[id], id == selectedId);
     // head
     id++;
     glTranslatef(0.0, 1.0, 0.0);
-    drawBox(2.0, 2.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.0, 2.0, orientations[id], id == selectedId);
     // left upper arm
     id++;
     glPopMatrix();
     glPushMatrix();
     glTranslatef(-1.75, 2.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.0, 1.0, orientations[id], id == selectedId);
     // left lower arm
     id++;
     glTranslatef(-2.0, 0.0, 0.0);
-    drawBox(1.5, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.5, 1.0, orientations[id], id == selectedId);
     // left hand
     id++;
     glTranslatef(-1.5, 0.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.5, 1.5, orientations[id], id == selectedId);
     // right upper arm
     id++;
     glPopMatrix();
     glTranslatef(1.75, 2.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(2.0, 1.0, orientations[id], id == selectedId);
     // right lower arm
     id++;
     glTranslatef(2.0, 0.0, 0.0);
-    drawBox(1.5, 1.0, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.5, 1.0, orientations[id], id == selectedId);
     // right hand
     id++;
     glTranslatef(1.5, 0.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id], id == selectedId);
+    glRotatef(rotations[id], 0.0, 0.0, 1.0);
+    drawBox(1.5, 1.5, orientations[id], id == selectedId);
 
     glutSwapBuffers();
 }
@@ -199,6 +223,14 @@ void keyboard(unsigned char key, int x, int y)
         else {
             selectedId = 15;
         }
+    }
+
+    if (key == 'A' || key == 'a') {
+        rotations[selectedId] = rotations[selectedId] + PI / 8;
+    }
+
+    if (key == 'D' || key == 'd') {
+        rotations[selectedId] = rotations[selectedId] - PI / 8;
     }
 
     glutPostRedisplay();
