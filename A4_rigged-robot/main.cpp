d22d1b9d8c80db866f12551fe2f7c66561033aae
Andrew Broderick <mjb1504@rit.edu>
2022-11-14 21:49:22 -0500

    added body part selection system

--- A4_rigged-robot/main.cpp
+++ A4_rigged-robot/main.cpp
@@ -3,8 +3,7 @@
 // Assignment 4
 
 //Usage:
-// Press 'W' to select the parent part.
-// Press 'S' to select a child part.
+// Press 'W' and 'S' to cycle through body parts
 // Press the left and right arrow keys to cycle the neighbor parts.
 // Press 'A' and 'D' to rotate the current selected part.
 
@@ -26,10 +25,8 @@ const float PI = 3.1415926;
 bool keyStates[256];
 int buttonState;
 
-float curMouse[2];
-float preMouse[2];
-
 int id = -1;
+int selectedId = 0;
 float rotations[16] = { PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, -PI / 2, PI / 2, PI / 2, PI / 2, PI, PI, PI, 0.0, 0.0, 0.0 };
 
 void init(void)
@@ -41,7 +38,7 @@ void init(void)
     buttonState = -1;
 }
 
-void drawBox(float boxWidth, float boxHeight, float rotation)
+void drawBox(float boxWidth, float boxHeight, float rotation, bool selected)
 {
     // vertices used for drawing
     float a[2] = { 0, 0 };
@@ -50,10 +47,13 @@ void drawBox(float boxWidth, float boxHeight, float rotation)
     float d[2] = { boxWidth, -1 * (boxHeight / 2.0) };
     float e[2] = { 0, -1 * (boxHeight / 2.0) };
 
-    // lines
+    // fill
     glColor3f(0.4, 0.8, 0.8);
     glLineWidth(3.0f);
     glBegin(GL_POLYGON);
+    if (selected) { // use a different fill color if the box is selected
+        glColor3f(0.2, 0.0, 0.8);
+    }
     glVertex2f(a[0] * cos(rotation) - a[1] * sin(rotation), a[0]*sin(rotation)+a[1]*cos(rotation));
     glVertex2f(b[0] * cos(rotation) - b[1] * sin(rotation), b[0] * sin(rotation) + b[1] * cos(rotation));
     glVertex2f(c[0] * cos(rotation) - c[1] * sin(rotation), c[0] * sin(rotation) + c[1] * cos(rotation));
@@ -61,7 +61,7 @@ void drawBox(float boxWidth, float boxHeight, float rotation)
     glVertex2f(e[0] * cos(rotation) - e[1] * sin(rotation), e[0] * sin(rotation) + e[1] * cos(rotation));
     glEnd();
 
-    // fill
+    // lines
     glColor3f(0.0, 0.0, 0.0);
     glLineWidth(3.0f);
     glBegin(GL_LINE_STRIP);
@@ -89,91 +89,77 @@ void display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    //int cid = -1; // the index of current circle
-    //// circle 0
-    //cid = 0;
-    //glTranslatef(translations[cid * 2 + 0], translations[cid * 2 + 1], 0.0f);
-    //glRotatef(rotations[cid], 0.0f, 0.0f, 1.0f);
-    //drawCircle(CIRCLE_RADIUM * (MAX_NUM_CIRCLE - cid) / MAX_NUM_CIRCLE, colors + cid * 3);
-
-    //// circle 1
-    //cid = 1;
-    //glTranslatef(translations[cid * 2 + 0], translations[cid * 2 + 1], 0.0f);
-    //glRotatef(rotations[cid], 0.0f, 0.0f, 1.0f);
-    //glPushMatrix(); // push the circle 1's CS to the modelview stack
-    //drawCircle(CIRCLE_RADIUM * (MAX_NUM_CIRCLE - cid) / MAX_NUM_CIRCLE, colors + cid * 3);
-
     //pelvis
     id = 0;
-    drawBox(2.5, 3.0, rotations[id]);
+    drawBox(2.5, 3.0, rotations[id], id == selectedId);
     glPushMatrix();
     // left thigh
     id++;
     glTranslatef(-1.0, 0.0, 0.0);
-    drawBox(3.0, 1.0, rotations[id]);
+    drawBox(3.0, 1.0, rotations[id], id == selectedId);
     // left calf
     id++;
     glTranslatef(0.0, -3.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id]);
+    drawBox(2.0, 1.0, rotations[id], id == selectedId);
     // left foot
     id++;
     glTranslatef(0.0, -2.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id]);
+    drawBox(1.5, 1.5, rotations[id], id == selectedId);
     // right thigh
     id++;
     glPopMatrix();
     glPushMatrix();
     glTranslatef(1.0, 0.0, 0.0);
-    drawBox(3.0, 1.0, rotations[id]);
+    drawBox(3.0, 1.0, rotations[id], id == selectedId);
     // right calf
     id++;
     glTranslatef(0.0, -3.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id]);
+    drawBox(2.0, 1.0, rotations[id], id == selectedId);
     // right foot
     id++;
     glTranslatef(0.0, -2.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id]);
+    drawBox(1.5, 1.5, rotations[id], id == selectedId);
     // chest 
     id++;
     glPopMatrix();
     glTranslatef(0.0, 2.5, 0.0);
-    drawBox(2.7, 3.5, rotations[id]);
+    drawBox(2.7, 3.5, rotations[id], id == selectedId);
     glPushMatrix();
     // neck
     id++;
     glTranslatef(0.0, 2.7, 0.0);
-    drawBox(1.0, 1.0, rotations[id]);
+    drawBox(1.0, 1.0, rotations[id], id == selectedId);
     // head
     id++;
     glTranslatef(0.0, 1.0, 0.0);
-    drawBox(2.0, 2.0, rotations[id]);
+    drawBox(2.0, 2.0, rotations[id], id == selectedId);
     // left upper arm
     id++;
     glPopMatrix();
     glPushMatrix();
     glTranslatef(-1.75, 2.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id]);
+    drawBox(2.0, 1.0, rotations[id], id == selectedId);
     // left lower arm
     id++;
     glTranslatef(-2.0, 0.0, 0.0);
-    drawBox(1.5, 1.0, rotations[id]);
+    drawBox(1.5, 1.0, rotations[id], id == selectedId);
     // left hand
     id++;
     glTranslatef(-1.5, 0.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id]);
+    drawBox(1.5, 1.5, rotations[id], id == selectedId);
     // right upper arm
     id++;
     glPopMatrix();
     glTranslatef(1.75, 2.0, 0.0);
-    drawBox(2.0, 1.0, rotations[id]);
+    drawBox(2.0, 1.0, rotations[id], id == selectedId);
     // right lower arm
     id++;
     glTranslatef(2.0, 0.0, 0.0);
-    drawBox(1.5, 1.0, rotations[id]);
+    drawBox(1.5, 1.0, rotations[id], id == selectedId);
     // right hand
     id++;
     glTranslatef(1.5, 0.0, 0.0);
-    drawBox(1.5, 1.5, rotations[id]);
+    drawBox(1.5, 1.5, rotations[id], id == selectedId);
 
     glutSwapBuffers();
 }
@@ -197,48 +183,30 @@ void keyboard(unsigned char key, int x, int y)
     if (key == 27) // 'esc' key
         exit(0);
 
+    if (key == 'W' || key == 'w') {
+        if (selectedId < 15) {
+            selectedId++;
+        }
+        else {
+            selectedId = 0;
+        }
+    }
+
+    if (key == 'S' || key == 's') {
+        if (selectedId > 0) {
+            selectedId--;
+        }
+        else {
+            selectedId = 15;
+        }
+    }
+
     glutPostRedisplay();
 }
 
 void keyboardUp(unsigned char key, int x, int y)
 {
-    unsigned char asciiOffset = 49; // see an ascii table
-
     glutPostRedisplay();
-}
-
-void mouse(int button, int state, int x, int y)
-{
-    if (state == GLUT_DOWN) {
-        buttonState = button;
-        curMouse[0] = ((float)x / win_width - 0.5f) * canvas_width;
-        curMouse[1] = ((float)(win_height - y) - 0.5f) / win_height * canvas_height;
-        preMouse[0] = ((float)x / win_width - 0.5f) * canvas_width;
-        preMouse[1] = ((float)(win_height - y) - 0.5f) / win_height * canvas_height;
-    }
-    else if (state == GLUT_UP)
-        button = -1;
-}
-
-void motion(int x, int y)
-{
-    unsigned char asciiOffset = 49; // see an ascii table
-
-    curMouse[0] = ((float)x / win_width - 0.5f) * canvas_width;
-    curMouse[1] = ((float)(win_height - y) - 0.5f) / win_height * canvas_height;
-
-    //else if (buttonState == GLUT_RIGHT_BUTTON) {
-    //    for (unsigned char i = '1'; i < '7'; i++) {
-    //        if (keyStates[i]) {
-    //            rotations[i - asciiOffset] += curMouse[0] - preMouse[0];
-    //        }
-    //    }
-    //    glutPostRedisplay();
-    //}
-
-    preMouse[0] = curMouse[0];
-    preMouse[1] = curMouse[1];
-
 }
 
 int main(int argc, char* argv[])
@@ -253,8 +221,6 @@ int main(int argc, char* argv[])
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutKeyboardUpFunc(keyboardUp);
-    glutMouseFunc(mouse);
-    glutMotionFunc(motion);
     glutMainLoop();
     return 0;
 
